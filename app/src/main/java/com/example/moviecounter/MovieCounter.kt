700eabdc5280c9b870bc1449095a6276a808d505
Nezaret <tu_correo@gmail.com>
2026-09-08 16:27:51 -0500

    feat: add static MovieCounter composable

---
+++ app/src/main/java/com/example/moviecounter/MovieCounter.kt
@@ -0,0 +1,34 @@
+package com.example.moviecounter
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun MovieCounter(modifier: Modifier = Modifier) {
+    val count = 0
+    Column(
+        modifier = modifier.padding(16.dp),
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text(text = "You have added $count movies.")
+        Spacer(modifier = Modifier.height(16.dp))
+        Button(onClick = { /* Acción del botón */ }) {
+            Text("Add Movie")
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun PreviewMovieCounter() {
+    MovieCounter()
+}
